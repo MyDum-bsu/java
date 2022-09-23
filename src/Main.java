2af510f267476caa3bd9f188a1e9ed9ff819b5dd
MyDum-bsu <mydum.bsu@gmail.com>
2022-09-23 20:46:56 +0300

    better codestyle

--- src/Main.java
+++ src/Main.java
@@ -1,12 +1,8 @@
 import lab1.Task7;
-import lab2.Matrix;
-
-import java.io.FileNotFoundException;
-import java.lang.reflect.InvocationTargetException;
 import java.util.Scanner;
 
 public class Main {
-    public static void main(String[] args) throws FileNotFoundException {
+    public static void main(String[] args) {
         System.out.println("to close the program type 'stop'");
         Scanner scanner = new Scanner(System.in);
         String str = scanner.nextLine();
