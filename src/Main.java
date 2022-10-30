66d879331a00fcd55ed3ee76beb4aea43e81d163
MyDum-bsu <mydum.bsu@gmail.com>
2022-10-30 19:20:28 +0300

    Teutonic Terror

--- src/Main.java
+++ src/Main.java
@@ -1,18 +1,9 @@
-import cw.Collection;
-import lab5.*;
 import lab6.*;
 
 public class Main {
     public static void main(String[] args) {
-        new TeleportingButtonApplication();
-        new MovingButtonApplication();
-        //        new DrawingFrame();
-//        Collection<Integer> collection = new Collection<>();
-//        for (int i = 0; i < 10; i++) {
-//            collection.add(i);
-//        }
-//
-//        System.out.println(collection.min());
-//        System.out.println(collection.max());
+        //new TeleportingButtonApplication();
+        //new MovingButtonApplication();
+        new DrawingFrame();
     }
 }
--- src/lab6/DrawingFrame.java
+++ src/lab6/DrawingFrame.java
@@ -2,58 +2,124 @@ package lab6;
 
 import javax.swing.*;
 import java.awt.*;
+import java.awt.event.MouseAdapter;
+import java.awt.event.MouseEvent;
+import java.awt.event.MouseMotionListener;
+import java.util.ArrayList;
 
 public class DrawingFrame extends JFrame {
-    private JRadioButton redButton;
-    private JRadioButton greenButton;
-    private JRadioButton blueButton;
 
-    private PanelHandler panel;
+    private final ArrayList<Line> lines = new ArrayList<>();
+    private final ArrayList<Line> bufferLines = new ArrayList<>();
+    private Color color = Color.RED;
+
+    JRadioButton redButton;
+    JRadioButton greenButton;
+    JRadioButton blueButton;
 
     public DrawingFrame() {
         super("Drawing Frame");
-        setDefaultCloseOperation(EXIT_ON_CLOSE);
-        setLayout(new GridLayout(2, 1, 0, 0));
-        setMinimumSize(new Dimension(600, 500));
+        initDefaultSet();
+        DrawingFrameMouseHandler handler = new DrawingFrameMouseHandler();
+        addMouseMotionListener(handler);
+        addMouseListener(handler);
         initColorButtons();
-
-        panel = new PanelHandler();
-        JScrollPane scrollPane = new JScrollPane(panel);
-//        scrollPane.setHorizontalScrollBarPolicy(JScrollPane.HORIZONTAL_SCROLLBAR_AS_NEEDED);
-//        scrollPane.setVerticalScrollBarPolicy(JScrollPane.VERTICAL_SCROLLBAR_NEVER);
-//        scrollPane.setBounds(50, 30, 300, 50);
-//        JPanel contentPane = new JPanel(null);
-//        contentPane.setPreferredSize(new Dimension(500, 400));
-//        contentPane.add(scrollPane);
-//        setContentPane(contentPane);
-//        pack();
-        add(scrollPane);
         setVisible(true);
     }
 
-    private void initColorButtons() {
-        JPanel panel = new JPanel(new FlowLayout());
-        redButton = new JRadioButton("red");
-        redButton.setSelected(true);
-        greenButton = new JRadioButton("green");
-        blueButton = new JRadioButton("blue");
-
-        ButtonGroup group = new ButtonGroup();
-        group.add(redButton);
-        group.add(greenButton);
-        group.add(blueButton);
-
-        panel.add(redButton);
-        panel.add(greenButton);
-        panel.add(blueButton);
-        add(panel);
+    private void initDefaultSet() {
+        setDefaultCloseOperation(EXIT_ON_CLOSE);
+        Dimension dimension = Toolkit.getDefaultToolkit().getScreenSize();
+        setBounds(dimension.width / 2 - 300, dimension.height / 2 - 150, 600, 300);
+        setMinimumSize(new Dimension(600, 300));
+        getContentPane().setBackground(Color.BLACK);
     }
 
-    private class PanelHandler extends JPanel {
-        @Override
-        public void paint(Graphics graphics) {
-            super.paint(graphics);
+    private void initColorButtons() {
+        ButtonGroup buttonGroup = new ButtonGroup();
+        redButton = new JRadioButton("red");
+        greenButton = new JRadioButton("green");
+        blueButton = new JRadioButton("blue");
+        JPanel colorPanel = new JPanel(new FlowLayout());
+        colorPanel.setBackground(Color.BLACK);
+        buttonGroup.add(redButton);
+        redButton.setSelected(true);
+        buttonGroup.add(greenButton);
+        buttonGroup.add(blueButton);
+
+        colorPanel.add(redButton);
+        colorPanel.add(greenButton);
+        colorPanel.add(blueButton);
+        add(colorPanel);
+    }
+
+    private Color getColor() {
+        if (redButton.isSelected()) {
+            return Color.RED;
+        } else if (greenButton.isSelected()) {
+            return Color.GREEN;
+        }
+        return Color.BLUE;
+    }
+
+    @Override
+    public void paint(Graphics graphics) {
+        super.paint(graphics);
+        color = getColor();
+        graphics.setColor(color);
+        for (Line line : lines) {
+            line.drawLine(graphics);
+        }
+    }
+
+    @Override
+    public void update(Graphics graphics) {
+        color = getColor();
+        graphics.setColor(color);
+        for (Line bufferLine : bufferLines) {
+            bufferLine.drawLine(graphics);
+        }
+        bufferLines.clear();
+    }
+
+    private static class Line {
+        private final Point a;
+        private final Point b;
+
+        private void drawLine(Graphics g) {
+            g.drawLine(a.x, a.y, b.x, b.y);
         }
 
+        Line(Point a, Point b) {
+            this.a = a;
+            this.b = b;
+        }
+    }
+
+    private class DrawingFrameMouseHandler extends MouseAdapter implements MouseMotionListener {
+        private Point lastPoint;
+
+        @Override
+        public void mouseDragged(MouseEvent mouseEvent) {
+            addLine(mouseEvent.getPoint());
+            lastPoint = mouseEvent.getPoint();
+        }
+
+        @Override
+        public void mousePressed(MouseEvent mouseEvent) {
+            lastPoint = mouseEvent.getPoint();
+        }
+
+        @Override
+        public void mouseReleased(MouseEvent mouseEvent) {
+            lastPoint = null;
+        }
+
+        private void addLine(Point point) {
+            Line line = new Line(lastPoint, point);
+            lines.add(line);
+            bufferLines.add(line);
+            update(getGraphics());
+        }
     }
 }
